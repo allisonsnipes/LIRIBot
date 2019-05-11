39ce14219c6b07aface2e882cf6e6075af265633
Allison Snipes <ajsnipes@usfca.edu>
2019-05-11 08:27:34 -0700

    setting up files

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+//grab the needed libraries
+var axios = require("axios");
+var Spotify = require('node-spotify-api');
+
+//define functions
+function LIRI() {
+  axios.get
+}
