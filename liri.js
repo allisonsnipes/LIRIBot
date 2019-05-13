931432b4e647e16b391bfa99eecc7509c4a9f6ff
Allison Snipes <ajsnipes@usfca.edu>
2019-05-12 17:01:36 -0700

    working on files

--- liri.js
+++ liri.js
@@ -1,15 +1,16 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
-
-var spotify = new Spotify(keys.spotify);
-
-//grab the needed libraries
+//grab the needed libraries and initialize them as variables
 var axios = require("axios");
 var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
 
 //define functions
 function LIRI() {
   axios.get
 }
+
+//I chose to work with Concert This
+
 
