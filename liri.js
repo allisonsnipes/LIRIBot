130b1684e65be4386fdcf9298eaf0167e8a7ad86
Allison Snipes <ajsnipes@usfca.edu>
2019-05-17 20:04:21 -0700

    got this baby working

--- liri.js
+++ liri.js
@@ -1,68 +1,69 @@
 //required to make program work and grab the needed libraries and initialize them as variables
 require("dotenv").config();
-
-//allows asychron file systems
-var fs = require("fs");
-
-//require usage of axios
-var axios = require("axios");
-
-//need keys
-var keys = require("./keys.js");
+var fs = require("fs");//allows asychron file systems
+var axios = require("axios");//require usage of axios
+var keys = require("./keys.js");//need keys
+//var moment = require("moment");//need moment.js api for time feature
 
 //require spotify api packages
-var Spotify = require("node-spotify-api");
-var spotify = new Spotify("keys.spotify");
-
-var request = require("request");
-//need moment.js api for time feature
-var moment = require("moment");
-
-//require usage of request
-var request = require("request");
-
-//need ombd api
-var ombd = require("omdb");
-
-//need bandsintown api
-var bandsintown = require("bandsintown")
+// var Spotify = require("node-spotify-api");
+// var spotify = new Spotify("keys.spotify");//doesnt matter to use var/const/let use let or const
 
 //be able to take in user input to get needed info
-var userInput = process.argv[2];
-var userInputQuery = process.argv.slice(3).join(" ")
+var action = process.argv[2];//command from user 'what to look up'
+var query = process.argv.slice(3).join(" ") //word to look unp
 
 //functions for program using case switch instead of if else statements
 //switch statement used to perform diferent actions based on different conditions
 //userInput is hardcoded set commands to run the function and the inputUserQuery is random input from user
 //function for command
-function runCommand(userInput, userInputQuery) {
-  switch (userInput) {
-    case "concert-this":
-      concertThis();
-      break;
-    case "spotify-this":
-      spotifyThisSong();
-      break;
-    case "movie-this":
-      movieThis();
-      break;
-    case "do-this":
-      doThis(userInputQuery);
-      break;
-    default:
-      console.log("Please search another term");
-      break;
-  }
+
+switch (action) {
+  case "concert-this":
+    concertThis(query);
+    break;
+  case "spotify-this":
+    spotifyThisSong();
+    break;
+  case "movie-this":
+    movieThis(query);
+    break;
+  case "do-this":
+    doThis(query);
+    break;
+  default:
+    console.log("Please search another term");
+    break;
 }
-runCommand(userInput, userInputQuery);
 
 //function for concertThis
 function concertThis() {
-  //template literal to help display data
-  console.log(`\n - - - - - \n\n ${userInputQuery}'s next concert is: `);
-  //use request here as a query url with the specified parameters from docs and userInputQuery
-  request("https://rest.bandsintown.com/artist/" + userInputQuery + "/events?app_id=" + bandsintown, function (error, response, body) {
-    
-  } )
-  
+  console.log("searching for concerts");
+  var URL = `https://rest.bandsintown.com/artists/${query}/events?app_id=codingbootcamp`;
+  axios.get(URL) //promise
+  .then(function(response) {//pass in callback funciton to do what we want once get back
+    //console.log(response.data); //response name doesnt matter its a parameter short res / resp
+
+  for (var i = 0; i < response.data.length; i++) {
+    console.log("----");
+    console.log("venue: " + response.data[i].venue.name);
+    console.log("city: " + response.data[i].venue.city);
+    console.log("region: " + response.data[i].venue.region);
+    console.log("date/time: " + response.data[i].datetime);
+    console.log("----");
+  } 
+
+  })
 }
+
+function movieThis() {
+ console.log("searching for movies");
+ var URL = `http://www.omdbapi.com/?apikey=trilogy&t=${query}`;
+ axios.get(URL) //promise
+   .then(function (response) { //pass in callback funciton to do what we want once get back
+      console.log(response.data); //response name doesnt matter its a parameter short res / resp
+      console.log(response.data.Title);
+      console.log(response.data.Year);
+      console.log(response.data.Actors);
+   })
+ }
