28f0c5c33790e998856656e8a7a9d22823871ba6
Allison Snipes <ajsnipes@usfca.edu>
2019-05-22 06:10:42 -0700

    done work on readme.md

--- liri.js
+++ liri.js
@@ -14,79 +14,69 @@ var spotify = new Spotify(keys.spotify);
 var action = process.argv[2];//command from user 'what to look up'
 var query = process.argv.slice(3).join(" "); //word to look unp
 
-// spotify.search({
-//   type: 'track',
-//   query: 'All the Small Things'
-// }, function (err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
-
-//   console.log(data);
-// });
-
 //functions for program using case switch instead of if else statements
 //switch statement used to perform diferent actions based on different conditions
 //userInput is hardcoded set commands to run the function and the inputUserQuery is random input from user
 
 //function for command
+
 function commands(action, query) {
   switch (action) {
     case "concert-this":
-      //concertThis(query);
+      concertThis(query);
       break;
     case "spotify-this":
       spotifyThisSong(query);
       break;
     case "movie-this":
-      //movieThis(query);
+      movieThis(query);
       break;
     case "do-this":
-      //doThis(query);
+      doThis(query);
       break;
     default:
-      console.log("Please search another term");
+      console.log("Please enter a specific command: concert-this, spotify-this, movie-this, or do-this.");//error handling
       break;
   }
 }
 commands(action, query);
 
 //function for concertThis
-// function concertThis() {
-//   console.log("searching for concerts");
-//   var URL = `https://rest.bandsintown.com/artists/${query}/events?app_id=codingbootcamp`;
-//   axios.get(URL) //promise
-//   .then(function(response) {//pass in callback funciton to do what we want once get back
-//     //console.log(response.data); //response name doesnt matter its a parameter short res / resp
+function concertThis() {
+  console.log("searching for concerts");
+  var URL = `https://rest.bandsintown.com/artists/${query}/events?app_id=codingbootcamp`;
+  axios.get(URL) //promise
+  .then(function(response) {//pass in callback funciton to do what we want once get back
+    //console.log(response.data); //response name doesnt matter its a parameter short res / resp
 
-//     for (var i = 0; i < response.data.length; i++) {
-//       console.log("----");
-//       console.log("venue: " + response.data[i].venue.name);
-//       console.log("city: " + response.data[i].venue.city);
-//       console.log("region: " + response.data[i].venue.region);
-//       console.log("date/time: " + response.data[i].datetime);
-//       console.log("----");
-//     } 
-//   })
-//   .catch(function(err) {
-//     console.log(err);
-//   })
-// }
+    for (var i = 0; i < response.data.length; i++) {
+      console.log("----");
+      console.log("venue: " + response.data[i].venue.name);
+      console.log("city: " + response.data[i].venue.city);
+      console.log("region: " + response.data[i].venue.region);
+      console.log("date/time: " + response.data[i].datetime);
+      console.log("----");
+    } 
+  })
+  .catch(function(err) {
+    console.log(err);
+  })
+}
 
-// function movieThis() {
-//  console.log("searching for movies");
-//  var URL = `http://www.omdbapi.com/?apikey=trilogy&t=${query}`;
-//  axios.get(URL) //promise
-//    .then(function (response) { //pass in callback funciton to do what we want once get back
-//       console.log(response.data); //response name doesnt matter its a parameter short res / resp
-//       console.log(response.data.Title);
-//       console.log(response.data.Year);
-//       console.log(response.data.Actors);
-//    })
-//    .catch(function (err) {
-//      console.log(err);
-//     })
-//  }
+function movieThis() {
+ console.log("searching for movies");
+ var URL = `http://www.omdbapi.com/?apikey=trilogy&t=${query}`;
+ axios.get(URL) //promise
+   .then(function (response) { //pass in callback funciton to do what we want once get back
+      console.log(response.data); //response name doesnt matter its a parameter short res / resp
+      console.log(response.data.Title);
+      console.log(response.data.Year);
+      console.log(response.data.Actors);
+   })
+   .catch(function (err) {
+     console.log(err);
+    })
+ }
 
 function spotifyThisSong() {
   console.log(`searching spotify for ${query}`);
@@ -103,25 +93,25 @@ function spotifyThisSong() {
     
     for (i = 0; i < arr.length; i++) {
       console.log("----");
-      //console.log(`artist: ${song.artists[i].name}`);
+      console.log(`artist: ${arr[i].artists[0].name}`);
       console.log(`song: ${arr[i].name}`);
-      // console.log(`album: ${song.album.name}`);
-      // console.log(`spotify link: ${song.preview_url}`);
+      console.log(`album: ${arr[i].album.name}`);
+      console.log(`spotify link: ${arr[i].preview_url}`);
       console.log("----"); 
     }
   })
 };
 
 //need to access random.txt file
-// function doThis() {
-//   fs.readFile("random.txt", "utf8", function (err, response) {
-//     if (err) {
-//       console.log(err);
-//     }
-//     var dArray = data.split(",");
-//     var action = dArray[0]; //take objects from txt file to use as parameters
-//     var query = dArray[1];
+function doThis() {
+  fs.readFile("random.txt", "utf8", function (err, response) {
+    if (err) {
+      console.log(err);
+    }
+    var dArray = data.split(",");
+    var action = dArray[0]; //take objects from txt file to use as parameters
+    var query = dArray[1];
 
-//     commands(action, query)
-//   })
-// }
+    commands(action, query)
+  })
+}
